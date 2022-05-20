48a282aa396e9674525cf5aaacba32141eb7b8f9
Nastya Lutsko <nastyalutsko777@gmail.com>
2022-05-20 20:49:22 +0200

    задание1

---
+++ js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+const galleryMarkUp = createElement(galleryItems);
+
+galleryList.insertAdjacentHTML('afterbegin', galleryMarkUp);
+galleryList.addEventListener("click", onGalleryListClick);
+// galleryList.document.addEventListener('keydown', (e) => {
+//   if (e.key === 'Escape') {
+//     instance.close();
+//   }
+// })
+function createElement(galleryItems){ 
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+function onGalleryListClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  } else {
+    const instance = basicLightbox.create(`<img src= "${e.target.dataset.source}" alt="${e.target.alt}"/>`);
+    instance.show()
+  } 
+  console.log(e.target);
+}
+
